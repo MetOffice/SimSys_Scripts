1e67eebe0c0b9026de670529814e7571d8271617
R Sharp <roddy.sharp@metoffice.gov.uk>
2023-10-20 13:01:20 +0100

    last bit of style changes

--- script_updater.sh
+++ script_updater.sh
@@ -8,8 +8,10 @@
 
 #set -eu
 
-github_url="git@github.com:MetOffice/SimSys_Scripts.git"
+git_repos="SimSys_Scripts.git"
+github_url="git@github.com:MetOffice/$git_repos"
 git_branch="add_umdp3checker_script"
+#git_branch="main"
 clone_destination="${CYLC_SUITE_SHARE_DIR}/imported_github_scripts"
 
 
@@ -21,12 +23,12 @@ if [[ $? != 0 ]]; then
     echo "  \"$clone_destination\""
     exit 1
   else
-    echo "Successfully removed old simulation-systems git directory"
+    echo "Successfully removed old ${git_repos%.git} git directory"
     echo "  \"$clone_destination\""
 fi
 
 # Mildly circular dependency here... The name of the branch needs changing -
-# once the branch has been committed..
+# once ths branch has been committed.. See commented out 'branch' name above.
 git clone -b $git_branch --single-branch $github_url "$clone_destination" 2>&1
 if [[ $? != 0 ]]; then
     echo -e "\nUnable to clone remote git repo into specified location."
