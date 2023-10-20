2cd194cd8da141db7dbcc39bfc3568b91cadd9c5
R Sharp <roddy.sharp@metoffice.gov.uk>
2023-10-20 12:09:37 +0100

    Quick tidy and stylistic tweaks

--- script_updater.sh
+++ script_updater.sh
@@ -8,9 +8,7 @@
 
 #set -eu
 
-github_url="https://github.com/MetOffice/SimSys_Scripts.git"
 github_url="git@github.com:MetOffice/SimSys_Scripts.git"
-git_branch="Lucy_testing_um_scripts"
 git_branch="add_umdp3checker_script"
 clone_destination="${CYLC_SUITE_SHARE_DIR}/imported_github_scripts"
 
@@ -20,25 +18,26 @@ echo "Git-scripts updater has started running"
 rm -rf "${CYLC_SUITE_SHARE_DIR}/imported_github_scripts"
 if [[ $? != 0 ]]; then
     echo "Couldn't remove specified folder. Try checking permissions"
-    echo "  \"${CYLC_SUITE_SHARE_DIR}/imported_github_scripts\""
+    echo "  \"$clone_destination\""
     exit 1
   else
     echo "Successfully removed old simulation-systems git directory"
-    echo "  \"${CYLC_SUITE_SHARE_DIR}/imported_github_scripts\""
+    echo "  \"$clone_destination\""
 fi
 
 # Mildly circular dependency here... The name of the branch needs changing -
 # once the branch has been committed..
-git clone -b $git_branch --single-branch $github_url "${CYLC_SUITE_SHARE_DIR}/imported_github_scripts" 2>&1
+git clone -b $git_branch --single-branch $github_url "$clone_destination" 2>&1
 if [[ $? != 0 ]]; then
-    echo "Unable to clone remote git repo into specified location."
-    echo "Check git branch, git url, destination path, permissions and git access"
+    echo -e "\nUnable to clone remote git repo into specified location."
+    echo "  Check git branch, git url, git access as well as"
+    echo -e "  destination path, and permissions\n"
     echo "GitHub url       = \"$github_url\""
     echo "git branch       = \"$git_branch\""
     echo "destination path = \"$clone_destination\""
     exit 1
   else
-    echo "Git repo successfully cloned"
+    echo -e "\nGit repo successfully cloned\n"
 fi
 
-echo "Github scripts updated"
+echo -e "\nGithub scripts updated\n"
