0dd4cc535b78a75341f600900338901aa7114bb1
James Bruten <james.bruten@metoffice.gov.uk>
2024-01-19 12:32:11 +0000

    initial copy from UM - no further changes

---
+++ kgo_updates/kgo_update/meto_run_kgo_script.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+# *****************************COPYRIGHT*******************************
+# (C) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT.txt
+# which you should have received as part of this distribution.
+# *****************************COPYRIGHT*******************************
+
+# This script is intended to be run by frum having been called by 
+# `../meto_update_kgo.sh` . It runs the `kgo_update.py` script which it
+# assumes is in the same directory. It takes suite name, suite user, version 
+# number, ticket number and required platforms as inputs. It runs the script on
+# each platform as required and then moves the generated variables file to
+# ~frum/kgo_updated_variables/vnVV.V_tTTTT/ on linux. IF xc40 is being run it
+# rsyncs with the xcs.
+# This script is NOT intended to be run independently of '../meto_update_kgo.sh'
+
+# Set colour codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Read in command line arguments
+while getopts S:U:E:N:R:P:V: flag
+do
+    case "${flag}" in
+        S) suite_name=${OPTARG};;
+        U) suite_user=${OPTARG};;
+        E) suite_user_ex1a=${OPTARG};;
+        N) new_kgo_dir=${OPTARG};;
+        R) new_release=${OPTARG};;
+        P) platforms=${OPTARG};;
+        V) version_number=${OPTARG};;
+    esac
+done
+
+# Create command to run python script
+kgo_command="./kgo_update.py"
+if [ $new_release -eq 1 ]; then
+    kgo_command="${kgo_command} --new-release"
+fi
+kgo_command="${kgo_command} -S $suite_name -N $new_kgo_dir"
+kgo_command_spice="$kgo_command -U $suite_user -P spice"
+kgo_command_xc40="$kgo_command -U $suite_user -P xc40"
+kgo_command_ex1a="$kgo_command -U $suite_user_ex1a -P ex1a"
+
+# Make a directory to store the script and variables.rc file in
+variables_dir=~/kgo_update_files/vn$version_number/$new_kgo_dir
+mkdir -p $variables_dir
+
+# If Spice has kgo updates
+if [[ $platforms == *"spice"* ]]; then
+    printf "${GREEN}\n\nRunning KGO Update Script on SPICE.\n${NC}"
+
+    # Run the Update Script
+    $kgo_command_spice
+
+    if [[ $? -ne 0 ]]; then
+        printf "${RED}\nThe installation script has failed on spice.\n${NC}"
+    else
+        # Move the updated variables file and script into the ticket folder
+        printf "${GREEN}\n\nSuccessfully installed on Spice.\n${NC}"
+        printf "${GREEN}Moving the generated files into SPICE ${variables_dir}.${NC}\n"
+        if [ $new_release -ne 1 ]; then
+            mv ~/variables.rc_${new_kgo_dir} ${variables_dir}/spice_updated_variables.rc
+        fi
+        mv ~/kgo_update_${new_kgo_dir}.sh ${variables_dir}/spice_update_script.sh
+    fi
+fi
+
+# If xc40 has kgo updates
+if [[ $platforms == *"xc40"* ]]; then
+    printf "${GREEN}\n\nRunning KGO Update Script on xc40.\n${NC}"
+
+    # SCP the python script to the xc40
+    scp -q kgo_update.py frum@xcel00:~
+
+    # Define the commands to run on xc40
+    command=". /etc/profile ; module load scitools ; 
+             ${kgo_command_xc40}"
+
+    # SSH to the xc40 with the run command
+    ssh -Y xcel00 $command
+
+    if [[ $? -ne 0 ]]; then
+        printf "${RED}\nThe installation script has failed on xc40.\n${NC}"
+    else
+        # rsync the generated variables file and script back to frum on linux
+        # This cleans up the original files
+        printf "${GREEN}\n\nSuccessfully installed on xc40.\n${NC}"
+        printf "${GREEN}Rsyncing the generated files into SPICE ${variables_dir}.\n${NC}"
+        if [ $new_release -ne 1 ]; then
+            rsync --remove-source-files -avz \
+                frum@xcel00:~/variables.rc_${new_kgo_dir} \
+                ${variables_dir}/xc40_updated_variables.rc
+        fi
+        rsync --remove-source-files -avz \
+                frum@xcel00:~/kgo_update_${new_kgo_dir}.sh \
+                ${variables_dir}/xc40_update_script.sh
+    fi
+
+    # Clean up kgo_update.py on xc40
+    ssh -Yq xcel00 "rm kgo_update.py"
+fi
+
+# If ex1a has kgo updates
+if [[ $platforms == *"ex1a"* ]]; then
+    printf "${GREEN}\n\nRunning KGO Update Script on ex1a.\n${NC}"
+
+    # SCP the python script to the ex1a
+    scp -q kgo_update.py umadmin@login.exz:~
+
+    # Define the commands to run on ex1a
+    command="module load scitools ; 
+             ${kgo_command_ex1a}"
+
+    # SSH to the ex1a with the run command
+    ssh -Y login.exz $command
+
+    if [[ $? -ne 0 ]]; then
+        printf "${RED}\nThe installation script has failed on ex1a.\n${NC}"
+    else
+        # rsync the generated variables file and script back to frum on linux
+        # This cleans up the original files
+        printf "${GREEN}\n\nSuccessfully installed on ex1a.\n${NC}"
+        printf "${GREEN}Rsyncing the generated files into SPICE ${variables_dir}.\n${NC}"
+        if [ $new_release -ne 1 ]; then
+            rsync --remove-source-files -avz \
+                    umadmin@login.exz:~/variables.rc_${new_kgo_dir} \
+                    ${variables_dir}/ex1a_updated_variables.rc
+        fi
+        rsync --remove-source-files -avz \
+                umadmin@login.exz:~/kgo_update_${new_kgo_dir}.sh \
+                ${variables_dir}/ex1a_update_script.sh
+    fi
+
+    # Clean up kgo_update.py on ex1a
+    ssh -Yq login.exz "rm kgo_update.py"
+fi
---
+++ kgo_updates/meto_update_kgo.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+# *****************************COPYRIGHT*******************************
+# (C) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT.txt
+# which you should have received as part of this distribution.
+# *****************************COPYRIGHT*******************************
+
+# This script is intended to be run from your_working_copy/rose-stem/bin. It 
+# asks for kgo update information then moves the directory kgo_update to ~frum.
+# It then calls `run_kgo_script.sh` as frum which itself calls the update script.
+# It then moves the generated variables files back into the working copy.
+# The kg_update.py script can be run with the --new-release option by providing
+# 'new-release' as a command line option to this script
+
+# Set colour codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Check that the Current Working Directory is in admin/rose-stem
+cwd=$(pwd)
+if [[ $cwd != *"admin/rose-stem" ]]; then
+    printf "${RED}This script must be run from a UM working copy: admin/rose-stem.\n"
+    printf "Please move to this folder and run again.${NC}\n"
+    exit 1
+fi
+
+# Check for command line argument to run with new release_mode
+new_release=0
+if [ $# -ne 0 ]; then
+    if [[ $1 == *"new-release"* ]]; then
+        new_release=1
+    fi
+fi
+
+# Prompt user for Update Details
+echo "Enter the platforms requiring a kgo update"
+echo "Enter platforms lowercase and space separated, eg. spice xc40 ex1a"
+read platforms
+if [[ $platforms == *"spice"* ]] || [[ $platforms == *"xc40"* ]]; then
+    read -p "SPICE/XC40 Suite Username: " suite_user
+else
+    suite_user=None
+fi
+if [[ $platforms == *"ex1a"* ]]; then
+    read -p "EX1A Suite Username: " suite_user_ex1a
+else
+    suite_user_ex1a=None
+fi
+read -p "Suite Name: " suite_name
+read -p "Version Number (VV.V): " version_number
+read -p "Ticket Number (TTTT): " ticket_number
+read -p "How should the new kgo directory be named (default vn${version_number}_t${ticket_number}): " new_kgo_dir
+new_kgo_dir=${new_kgo_dir:-"vn${version_number}_t${ticket_number}"}
+
+# Get user to double check settings
+clear
+echo "Suite Name: ${suite_name}"
+if [[ $platforms == *"spice"* ]] || [[ $platforms == *"xc40"* ]]; then
+    echo "User: ${suite_user}"
+fi
+if [[ $platforms == *"ex1a"* ]]; then
+    echo "EX1A User: ${suite_user_ex1a}"
+fi
+echo "Version Number: ${version_number}"
+echo "Ticket Number: ${ticket_number}"
+echo "New KGO Dir: ${new_kgo_dir}"
+if [ $new_release -eq 1 ]; then
+    echo "WARNING: Running with --new-release enabled"
+fi
+read -p "Run with the above settings y/n (default n): " run_script
+run_script=${run_script:-n}
+if [ $run_script != "y" ]; then
+    exit 0
+fi
+
+# Move the kgo_update directory to frum on linux
+scp -rq kgo_update frum@localhost:~
+
+# Define command to run as frum
+command=". /etc/profile ; module load scitools ; cd kgo_update ;
+         ./meto_run_kgo_script.sh -S ${suite_name}
+                                  -U ${suite_user}
+                                  -E ${suite_user_ex1a}
+                                  -N ${new_kgo_dir}
+                                  -R ${new_release}
+                                  -P '${platforms}' 
+                                  -V ${version_number} ;
+         cd ~ ; rm -rf kgo_update"
+
+# Run the command as frum
+ssh -Y frum@localhost $command
+
+# Error Checking and rsyncing
+variables_dir=kgo_update_files/vn${version_number}/${new_kgo_dir}
+succeeded_spice=0
+succeeded_xc40=0
+succeeded_ex1a=0
+succeeded_all=1
+if [[ $platforms == *"spice"* ]]; then
+    file=~frum/${variables_dir}/spice_update_script.sh
+    if [[ -e "$file" ]]; then
+        succeeded_spice=1
+        if [[ $new_release -ne 1 ]]; then
+            printf "${GREEN}\n\nCopying the spice variables file into this working copy.\n${NC}"
+            scp -q frum@localhost:~/${variables_dir}/spice_updated_variables.rc \
+                                        ../../rose-stem/site/meto/variables_spice.rc
+            if [[ $? -ne 0 ]]; then
+                printf "${RED}The copy of the spice variables.rc into this working copy has failed.\n${NC}"
+                succeeded_spice=0
+                succeeded_all=0
+            fi
+        fi
+    else
+        succeeded_all=0
+    fi
+fi
+if [[ $platforms == *"xc40"* ]]; then
+    file=~frum/${variables_dir}/xc40_update_script.sh
+    if [[ -e "$file" ]]; then
+        succeeded_xc40=1
+        if [[ $new_release -ne 1 ]]; then
+            printf "${GREEN}\n\nCopying the xc40 variables file into this working copy.\n${NC}"
+            scp -q frum@localhost:~/${variables_dir}/xc40_updated_variables.rc \
+                                        ../../rose-stem/site/meto/variables_xc40.rc
+            if [[ $? -ne 0 ]]; then
+                printf "${RED}The copy of the xc40 variables.rc into this working copy has failed.\n${NC}"
+                succeeded_xc40=0
+                succeeded_all=0
+            fi
+        fi
+    else
+        succeeded_all=0
+    fi
+fi
+if [[ $platforms == *"ex1a"* ]]; then
+    file=~frum/${variables_dir}/ex1a_update_script.sh
+    if [[ -e "$file" ]]; then
+        succeeded_ex1a=1
+        if [[ $new_release -ne 1 ]]; then
+            printf "${GREEN}\n\nCopying the ex1a variables file into this working copy.\n${NC}"
+            scp -q frum@localhost:~/${variables_dir}/ex1a_updated_variables.rc \
+                                        ../../rose-stem/site/meto/variables_ex1a.rc
+            if [[ $? -ne 0 ]]; then
+                printf "${RED}The copy of the ex1a variables.rc into this working copy has failed.\n${NC}"
+                succeeded_ex1a=0
+                succeeded_all=0
+            fi
+        fi
+    else
+        succeeded_all=0
+    fi
+fi
+
+if [[ $succeeded_all -eq 1 ]]; then
+    printf "${GREEN}All kgo has been successfully installed.\n${NC}"
+else
+    printf "${RED}\n\nAt least 1 platform suffered an error during installation.\n${NC}"
+fi
+
+# For the xc40s rsync the generated kgo to the xcs
+if [[ $succeeded_xc40 -eq 1 ]]; then
+    printf "${GREEN}\n\nrsyncing the kgo to xcs.\n${NC}"
+    rsync_com="ssh -Y xcel00 'rsync -av /projects/um1/standard_jobs/kgo/ xcslr0:/common/um1/standard_jobs/kgo/'"
+    ssh -Y frum@localhost $rsync_com
+    if [[ $? -ne 0 ]]; then
+        printf "${RED}The rsync to the xcs has failed.\n${NC}"
+    else
+        printf "${Green}The rsync to the xcs has succeeded.\n${NC}"
+    fi
+elif [[ $platforms == *"xc40"* ]]; then
+    printf "${RED}\n\nSkipping the rsync to the xcs as the xc40 install failed.\n${NC}"
+fi
+
+printf "\n\nInstallation Summary:\n\n"
+if [[ $platforms == *"spice"* ]]; then
+    if [[ $succeeded_spice -eq 1 ]]; then
+        printf "${GREEN}Installation on spice successful.\n${NC}"
+    else
+        printf "${RED}Installation on spice unsuccessful. Review output for error.\n${NC}"
+    fi
+fi
+if [[ $platforms == *"xc40"* ]]; then
+    if [[ $succeeded_xc40 -eq 1 ]]; then
+        printf "${GREEN}Installation on xc40 successful.\n${NC}"
+    else
+        printf "${RED}Installation on xc40 unsuccessful. Review output for error.\n${NC}"
+    fi
+fi
+if [[ $platforms == *"ex1a"* ]]; then
+    if [[ $succeeded_ex1a -eq 1 ]]; then
+        printf "${GREEN}Installation on ex1a successful.\n${NC}"
+    else
+        printf "${RED}Installation on ex1a unsuccessful. Review output for error.\n${NC}"
+    fi
+fi
