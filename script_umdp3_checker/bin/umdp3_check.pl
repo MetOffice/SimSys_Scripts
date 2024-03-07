48fcbf9c8ebfd1625f0e93d26fcced36757b67ee
AdamVoysey <48529888+AdamVoysey@users.noreply.github.com>
2024-03-07 17:16:06 +0000

    fix typo

--- script_umdp3_checker/bin/umdp3_check.pl
+++ script_umdp3_checker/bin/umdp3_check.pl
@@ -355,8 +355,8 @@ if ( $trunkmode == 0 ) {
         # enable trunkmode for sepecific repositories if the environment does not match rose-stem/rose-suite.conf
 
         my $ss_env = $ENV{SCRIPT_SOURCE};
-        my @suite_conf = cat_file($ss_env+/"rose-stem/rose-suite.conf");
-        my @host_sources = grep /^HOST_SOURCE_.*=/, @suite_conf;;
+        my @suite_conf = cat_file($ss_env+"/rose-stem/rose-suite.conf");
+        my @host_sources = grep /^HOST_SOURCE_.*=/, @suite_conf;
         
         print "Detected HOST_SOURCE variables:\n";
         print join( "\n", @host_sources );
