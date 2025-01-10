214f9748f97bee2bf2a6aae3f74a91805fed2f29
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2025-01-10 14:25:49 +0000

    change apply_macros to print macros to all files, including science (#48)
    * change apply_macros to print macros to all files, including science
    
    * add fix for 2nd macro in chain
    
    * comment fix and change to black filename

--- lfric_macros/apply_macros.py
+++ lfric_macros/apply_macros.py
@@ -745,7 +745,7 @@ class ApplyMacros:
                 "        return config, self.reports\n"
             )
 
-        run_black(filepath)
+        run_black(temppath)
 
         os.rename(temppath, filepath)
 
@@ -804,17 +804,7 @@ class ApplyMacros:
 
         # Now reconstruct the macro for all applications which have the newly
         # added macro or import metadata with the new macro
-        # Note, this is only done for applications, not for science sections
         for meta_dir in self.meta_dirs:
-            if not (
-                meta_dir.startswith(
-                    os.path.join(self.root_path, "applications")
-                )
-                or meta_dir.startswith(
-                    os.path.join(self.core_source, "applications")
-                )
-            ):
-                continue
             import_order = self.determine_import_order(meta_dir)
             full_command = self.combine_macros(import_order)
             # If there are commands to write out, do so and record this
