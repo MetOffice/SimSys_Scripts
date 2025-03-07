022f1dca4ee3538fc3130522beb34eaf8fd2f5df
cameronbateman-mo <cameron.bateman@metoffice.gov.uk>
2025-03-07 10:25:38 +0000

    added argument parsing for user input.

--- forking_styling.py
+++ forking_styling.py
@@ -1,6 +1,10 @@
 import re
 import sys
+import argparse
+import os
+from pathlib import Path
 from styling import KEYWORDS
+
 
 def lowercase_keywords(file):
     print("Lowercasing keywords in", file)
@@ -13,8 +17,28 @@ def lowercase_keywords(file):
         for line in lines:
             fp.write(line)
 
-def main():
+
+def convert_to_lower(match_obj):
+    if match_obj.group() is not None:
+        return match_obj.group().lower()
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-directory",
+        metavar="home/user/etc",
+        type=Path,
+        default=Path(),
+        help="path to a directory of files."
+    )
+
+    arguments = parser.parse_args()
+
+    for root, dirs, files in os.walk(arguments.directory):
+        for file in files:
+            if file.endswith((".f90", ".F90")):
+                cur_path = os.path.join(root, file)
+                lowercase_keywords(cur_path)
+
