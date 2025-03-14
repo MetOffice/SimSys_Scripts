b7d6cf6247179362a7926120d1a49fee355e4e4e
Cameron Bateman <cameron.bateman@metoffice.gov.uk>
2025-03-14 10:56:19 +0000

    UM to LFRic styling script. (#63)
    * Initial code copy from styling.py from simssy_scripts
    
    * removed import * from script
    
    * created fstring_parse.py module from um/trunk/admin/code_styling and moved into separate dir.
    
    * new code for uppercase to lowercase
    
    * added argument parsing for user input.
    
    * added argument parsing for user input.
    
    * first working pass.
    
    * added comments and user input checking.
    
    * renamed file.
    
    * removed print lines and added import back to styling.py
    
    * Delete .idea/.gitignore
    
    * removing .idea folder
    
    * added main() function to allow importing of module.
    
    * moved code block in main() to apply_styling().
    created new file styling_keywords.py to remove coupling to styling.py.
    added copyright statement.
    
    * Added docstrings to functions.
    
    * Added short and verbose options correctly.

---
+++ lfric_styling.py
@@ -0,0 +1,81 @@
+# *****************************COPYRIGHT*******************************
+# (C) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file LICENSE
+# which you should have received as part of this distribution.
+# Created date: 10/03/2025
+# Modified date: 14/03/2025
+# *****************************COPYRIGHT*******************************
+
+'''
+Python script to apply LFRic styling to .F90/.f90 files, given a path/directory.
+
+Current implementation takes a valid path and traverses from the top level file
+structure identifying fortran files, and applying styling.
+
+Styling currently being applied:
+    -lowercasing keywords
+
+'''
+
+import re
+import sys
+import argparse
+import os
+from pathlib import Path
+from styling_keywords import KEYWORDS
+
+
+def lowercase_keywords(file):
+    """
+    Lowercase words in a file when they match a word in the keywords set.
+    """
+    print("Lowercasing keywords in", file)
+    with open(file, 'r') as fp:
+        lines = fp.read()
+        for keyword in KEYWORDS:
+            pattern = rf"\b{re.escape(keyword.upper())}\b"
+            lines = re.sub(pattern, convert_to_lower, lines)
+    with open(file, 'w') as fp:
+        for line in lines:
+            fp.write(line)
+
+
+def convert_to_lower(match_obj):
+    """Checks if match is true and lowercases string."""
+    if match_obj.group() is not None:
+        return match_obj.group().lower()
+
+
+def apply_styling(path_to_dir):
+    """
+    Take a path to a directory containing fortran files.
+    Applying styling from other functions.
+    Identifying fortran files by extension.
+    """
+    if os.path.exists(path_to_dir):
+        for root, dirs, files in os.walk(path_to_dir):
+            for file in files:
+                if file.endswith((".f90", ".F90")):
+                    cur_path = os.path.join(root, file)
+                    lowercase_keywords(cur_path)
+    else:
+        print("ERROR: Path supplied does not exist or is incorrect.")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-d", "--directory",
+        metavar="home/user/etc",
+        type=Path,
+        default=Path(),
+        help="path to a directory of files."
+    )
+
+    arguments = parser.parse_args()
+    apply_styling(arguments.directory)
+
+
+if __name__ == '__main__':
+    main()
