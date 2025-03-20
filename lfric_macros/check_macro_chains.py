4d2417b990edbcdfa5956e5a5a5f0a7277a2d950
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2025-03-20 11:50:41 +0000

    Update lfric_macros/check_macro_chains.py
    Co-authored-by: Erica Neininger <107684099+ericaneininger@users.noreply.github.com>

--- lfric_macros/check_macro_chains.py
+++ lfric_macros/check_macro_chains.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ##############################################################################
-# (c) Crown copyright 2024 Met Office. All rights reserved.
+# (c) Crown copyright 2025 Met Office. All rights reserved.
 # The file LICENCE, distributed with this code, contains details of the terms
 # under which the code may be used.
 ##############################################################################
