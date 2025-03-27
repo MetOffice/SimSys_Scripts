9b046ef1347384ee7eb86eca58839cd49d974663
James Bruten <james.bruten@metoffice.gov.uk>
2025-03-27 08:37:04 +0000

    remove \ from f-string

--- lfric_macros/check_macro_chains.py
+++ lfric_macros/check_macro_chains.py
@@ -95,8 +95,7 @@ def compare_tags(before, after, path, errors):
             "after tag.\nThere should be 2 of these - the beginning of the "
             "chain and the end of the chain.\nThis is likely a typo in the tags in "
             "the versions.py file. The identified tags were:\n"
-            # f"{'\n'.join(x for x in single_tags)}"
-        )
+        ) + "\n".join(x for x in single_tags)
 
 
 def main():
