757f65ffa249afcbc907e595ec44aa626e5c461d
James Bruten <james.bruten@metoffice.gov.uk>
2025-09-16 17:09:56 +0100

    black

--- suite_report_git/suite_data.py
+++ suite_report_git/suite_data.py
@@ -47,12 +47,14 @@ class SuiteData:
         "atmos_thread",
         "-v-",
     )
+
     def __init__(self) -> None:
         self.dependencies = {}
         self.rose_data = {}
         self.suite_path = None
         self.task_states = {}
         self.temp_directory = None
+
     def __init__(self) -> None:
         pass
 
