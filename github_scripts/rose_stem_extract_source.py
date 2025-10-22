f0c6279ed2ec460c89e4e07826b2c3915b184148
James Bruten <james.bruten@metoffice.gov.uk>
2025-10-22 13:46:59 +0100

    add option for using https

--- github_scripts/rose_stem_extract_source.py
+++ github_scripts/rose_stem_extract_source.py
@@ -18,20 +18,44 @@ from get_git_sources import clone_repo, clone_repo_mirror, sync_repo
 from typing import Dict
 
 
+def set_https(dependencies: Dict) -> Dict:
+    """
+    Change sources in a dependencies dictions to use https instead of ssh
+    """
+
+    print("Modifying Dependencies")
+    for dependency, values in dependencies.items():
+        if values["source"].startswith("git@github.com:"):
+            source = dependencies[dependency]["source"]
+            dependencies[dependency]["source"] = source.replace(
+                "git@github.com:", "https://github.com/"
+            )
+
+    return dependencies
+
+
 def main() -> None:
 
     clone_loc = Path(os.environ["SOURCE_DIRECTORY"])
 
     dependencies: Dict = literal_eval(os.environ["DEPENDENCIES"])
 
+    if os.environ.get("USE_TOKENS", "False") == "True":
+        dependencies = set_https(dependencies)
+
     for dependency, values in dependencies.items():
 
-        print(f"Extracting {dependency} at time {datetime.now()}")
+        print(
+            f"Extracting {dependency} at time {datetime.now()} "
+            f"using source {values['source']} and ref {values['ref']}"
+        )
 
         loc = clone_loc / dependency
 
         if ".git" in values["source"]:
-            if os.environ["USE_MIRRORS"] == "True":
+            print(os.environ.get("USE_MIRRORS", "False"))
+            print(type(os.environ.get("USE_MIRRORS", "False")))
+            if os.environ.get("USE_MIRRORS", "False") == "True":
                 mirror_loc = Path(os.environ["GIT_MIRROR_LOC"]) / values["parent"]
                 clone_repo_mirror(
                     values["source"], values["ref"], values["parent"], mirror_loc, loc
