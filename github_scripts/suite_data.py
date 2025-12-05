781a25047b2e54c85e0de004d1655a55b07bcc1e
James Bruten <james.bruten@metoffice.gov.uk>
2025-12-05 09:51:14 +0000

    add default group

--- github_scripts/suite_data.py
+++ github_scripts/suite_data.py
@@ -341,6 +341,8 @@ class SuiteData:
             if row[0] in ("g", "group"):
                 groups = row[1].strip("[]'\"").split(",")
                 break
+        if groups is None:
+            groups = "suite_default"
         return groups
 
     def get_task_states(self) -> Dict[str, str]:
