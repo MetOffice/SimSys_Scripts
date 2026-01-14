b3fa7c59a49576319b576bbd5a722b240dfbe208
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2026-01-14 13:48:32 +0000

    remove break function

--- github_scripts/suite_data.py
+++ github_scripts/suite_data.py
@@ -312,7 +312,6 @@ class SuiteData:
                 match = re.search(r"INFO - Workflow: (\S+\/\w+)", line)
                 try:
                     workflow_id = match.group(1)
-                    break
                 except IndexError:
                     continue
 
