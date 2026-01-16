f39074fc5cab13d8716aefc2eb79e57c04255bb6
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2026-01-16 11:14:41 +0000

    bump timeout (#162)
    * bump timeout
    
    * test modifying
    
    * revert change
    
    ---------
    
    Co-authored-by: Erica Neininger <107684099+ericaneininger@users.noreply.github.com>

--- github_scripts/get_git_sources.py
+++ github_scripts/get_git_sources.py
@@ -23,7 +23,7 @@ def run_command(
     command: str,
     check: bool = True,
     capture: bool = True,
-    timeout: int = 300
+    timeout: int = 600
 ) -> Optional[subprocess.CompletedProcess]:
     """
     Run a subprocess command and return the result object
