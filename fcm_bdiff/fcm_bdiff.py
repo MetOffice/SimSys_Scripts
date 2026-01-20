31efcffadfbc05ae6501632838bfdeaaa0d5491a
R Sharp <r-sharp@users.noreply.github.com>
2026-01-20 15:06:49 +0000

    Update fcm_bdiff/fcm_bdiff.py
    Co-authored-by: Yaswant Pradhan <2984440+yaswant@users.noreply.github.com>

--- fcm_bdiff/fcm_bdiff.py
+++ fcm_bdiff/fcm_bdiff.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-# *********************************COPYRIGHT************************************
+# -----------------------------------------------------------------------------
 # (C) Crown copyright Met Office. All rights reserved.
-# For further details please refer to the file COPYRIGHT.txt
-# which you should have received as part of this distribution.
-# *********************************COPYRIGHT************************************
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+# -----------------------------------------------------------------------------
 """
 This module provides the functionality to return a list of local files to
 run tests on based on the branch-difference (to allow checking of only files
