a5022acab3f3a3f79e69517476d1d0845cacf5a2
R Sharp <r-sharp@users.noreply.github.com>
2026-01-20 15:09:01 +0000

    Update fcm_bdiff/fcm_bdiff.py
    Co-authored-by: Yaswant Pradhan <2984440+yaswant@users.noreply.github.com>

--- fcm_bdiff/fcm_bdiff.py
+++ fcm_bdiff/fcm_bdiff.py
@@ -347,4 +347,5 @@ class FCMInfo(FCMBase):
 
     def is_main(self) -> bool:
         """Return True if the branch is the main trunk."""
+        
         return self.is_trunk_test(self._branch_url)
