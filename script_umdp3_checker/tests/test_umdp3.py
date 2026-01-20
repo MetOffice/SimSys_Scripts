679620fa7cf201a5a5e0a78c7cba23358c002d46
R Sharp <r-sharp@users.noreply.github.com>
2026-01-20 15:09:16 +0000

    Update script_umdp3_checker/tests/test_umdp3.py
    Co-authored-by: Yaswant Pradhan <2984440+yaswant@users.noreply.github.com>

--- script_umdp3_checker/tests/test_umdp3.py
+++ script_umdp3_checker/tests/test_umdp3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-# *****************************COPYRIGHT*******************************
+# -----------------------------------------------------------------------------
 # (C) Crown copyright Met Office. All rights reserved.
-# For further details please refer to the file LICENSE
-# which you should have received as part of this distribution.
-# *****************************COPYRIGHT*******************************
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+# -----------------------------------------------------------------------------
 
 """
 Test script for the Python UMDP3 checker
