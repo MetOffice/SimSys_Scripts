de0bc74ee802882e0c3f6106bd82e4cc544964cc
jennyhickson <61183013+jennyhickson@users.noreply.github.com>
2026-02-02 14:26:48 +0000

    functional

--- gh_review_project/finish_milestone.py
+++ gh_review_project/finish_milestone.py
@@ -38,14 +38,14 @@ def still_open(open_prs: dict, current_milestone: str) -> int:
     for repo in open_prs:
         print(f"{repo} \n{'-'*len(repo)}")
         for pr in open_prs[repo]:
-            print(f"{pr['status']: <18} #{pr['number']: <5} {pr['title']}")
+            print(f"{pr.status: <18} #{pr.number: <5} {pr.title}")
 
         count = len(open_prs[repo])
         print(f"->   {count} open pull request(s) in {repo} \n")
         total += count
 
     if total == 0:
-        print("No open pull requests for this milestone \n")
+        print(f"No open pull requests for {current_milestone} \n")
 
     return total
 
@@ -66,7 +66,7 @@ def closed_other(closed_prs: dict, current_milestone: str) -> int:
         for repo in closed_prs[milestone]:
             print(f"{repo} \n{'-' * len(repo)}")
             for pr in closed_prs[milestone][repo]:
-                print(f"#{pr['number'] : <5} {pr['title']}")
+                print(f"#{pr.number : <5} {pr.title}")
 
             count = len(closed_prs[milestone][repo])
             print(
@@ -75,12 +75,20 @@ def closed_other(closed_prs: dict, current_milestone: str) -> int:
             total += count
 
     if total == 0:
-        print("No closed pull requests not at this milestone \n")
+        print(f"No closed pull requests not for {current_milestone} \n")
 
     return total
 
 
 def check_ready(open_prs: dict, closed_prs: dict, milestone: str) -> None:
+    """
+    Check if the milestone is ready to be closed by confirming that:
+      * all pull requests for this milestone have been completed
+      * all closed pull requests in the project are in this milestone.
+
+    Give the user the choice to continue regardless since there may be valid
+    exceptions.
+    """
     total_open = still_open(open_prs[milestone], milestone)
     total_other = closed_other(closed_prs, milestone)
 
@@ -90,31 +98,24 @@ def check_ready(open_prs: dict, closed_prs: dict, milestone: str) -> None:
             f"{total_open} open pull request(s) in {milestone} and "
             f"{total_other} closed pull request(s) not in {milestone}."
         )
-        # cont = input("Would you like to continue with closing this milestone? (y/n) ")
-        #
-        # if cont == "n":
-        #     exit(0)
-        # elif cont != "y":
-        #     print("Unrecognised input, please select y or n")
+        cont = input("Would you like to continue with closing this milestone? (y/n) ")
+
+        if cont == "n":
+            exit(0)
+        elif cont != "y":
+            print("Unrecognised input, please select y or n")
+
 
 def report(closed: dict, milestone: str) -> None:
+    """
+    Report on the pull requests completed in this milestone
+    """
 
     print_banner(f"Pull requests completed for {milestone}")
 
     for repo in closed:
         print(f"{repo: <20} {len(closed[repo]): >3} pull requests")
 
-
-def archive(closed_prs: dict, dryrun: bool) -> None:
-
-    print_banner("Archiving all completed pull requests for this milestone")
-
-    #gh project item-archive [<number>] [flags]
-
-    for repo in closed_prs:
-        for pr in closed_prs[repo]:
-            if dryrun:
-                print(f"Archiving #{pr['number']} in {repo}")
 
 def parse_args():
     """
@@ -142,14 +143,14 @@ def parse_args():
     parser.add_argument(
         "--file",
         default=testfile,
-        help="Filepath to test data for either capture the project status, "
+        help="Filepath to test data for either capturing the project status, "
         "or use as input data.",
     )
     parser.add_argument(
         "--dry",
         action="store_true",
         help="Dry run. Print commands, don't action them. Always true when "
-             "running with test data."
+        "running with test data.",
     )
 
     args = parser.parse_args()
@@ -163,8 +164,11 @@ def parse_args():
     return args
 
 
-def main(milestone: str, test: bool, capture_project: bool, file: Path, dry: bool) -> None:
+def main(
+    milestone: str, test: bool, capture_project: bool, file: Path, dry: bool
+) -> None:
 
+    # Get milestone data
     if test:
         data = ProjectData.from_file(file)
     else:
@@ -173,13 +177,20 @@ def main(milestone: str, test: bool, capture_project: bool, file: Path, dry: boo
     open_prs_by_milestones = data.get_by_milestone("open")
     closed_prs_by_milestones = data.get_by_milestone("closed")
 
+    # Process data and report on status
     check_ready(open_prs_by_milestones, closed_prs_by_milestones, milestone)
-
     report(closed_prs_by_milestones[milestone], milestone)
 
-    archive(closed_prs_by_milestones[milestone], dry)
+    # Archive pull requests at the milestone
+    print_banner(f"Archiving Milestone {milestone}")
+    data.archive_milestone(milestone, dry_run=dry)
 
-    print("pause")
+    # Close milestones
+    # TODO: run this command from here, rather than prompting user. Leaving
+    #  like this until script feels stable.
+    print_banner("Milestone Completed in Simulation Systems Review Tracker project")
+    print("Run this command to close the milestone in all repositories:")
+    print(f'./sbin/gh_manage_milestones -t "{milestone}" -m close')
 
 
 if __name__ == "__main__":
--- gh_review_project/review_project.py
+++ gh_review_project/review_project.py
@@ -5,7 +5,7 @@
 # -----------------------------------------------------------------------------
 
 """
-Class and functions for interacting with the Simulation Systems Review Tracker
+Classes and functions for interacting with the Simulation Systems Review Tracker
 Project.
 """
 
@@ -13,6 +13,18 @@ import json
 import subprocess
 from pathlib import Path
 from collections import defaultdict
+
+project_id = 376
+project_owner = "MetOffice"
+
+
+def run_command(command: str) -> subprocess.CompletedProcess:
+    output = subprocess.run(command.split(), capture_output=True, timeout=180)
+
+    if output.returncode:
+        raise RuntimeError(output.stderr.decode())
+
+    return output
 
 
 class ProjectData:
@@ -42,12 +54,8 @@ class ProjectData:
         """
         Retrieve data from GitHub API and initialise the class.
         """
-        command = "gh project item-list 376 -L 500 --owner MetOffice --format json"
-        output = subprocess.run(command.split(), capture_output=True, timeout=180)
-        if output.returncode:
-            raise RuntimeError(
-                "Error fetching GitHub Project data:  \n " + output.stderr.decode()
-            )
+        command = f"gh project item-list {project_id} -L 500 --owner {project_owner} --format json"
+        output = run_command(command)
 
         raw_data = json.loads(output.stdout)
 
@@ -74,50 +82,40 @@ class ProjectData:
         return cls(data=data, test=True, milestones=milestones)
 
     @classmethod
-    def _extract_data(cls, raw_data: dict) -> dict:
+    def _extract_data(cls, raw_data: dict) -> (dict, list):
         """
         Extract useful information from the raw data and
         store it in a dictionary keyed by repository.
         """
 
         data = defaultdict(list)
-        milestones = set()
+        milestones = set("None")
 
         for pr in raw_data["items"]:
-            pull_request = {}
-            pull_request["id"] = pr["id"]
-            pull_request["title"] = pr["content"]["title"]
-            pull_request["number"] = pr["content"]["number"]
+            pull_request = PullRequest(
+                id=pr["id"],
+                number=pr["content"]["number"],
+                title=pr["content"]["title"],
+            )
 
             if "status" in pr:
-                pull_request["status"] = pr["status"]
-            else:
-                pull_request["status"] = None
+                pull_request.status = pr["status"]
 
             if "milestone" in pr:
-                pull_request["milestone"] = pr["milestone"]["title"]
-            else:
-                pull_request["milestone"] = "None"
-
-            milestones.add(pull_request["milestone"])
+                pull_request.milestone = pr["milestone"]["title"]
+                milestones.add(pull_request.milestone)
 
             if "assignee" in pr:
-                pull_request["assignee"] = pr["assignees"]
-            else:
-                pull_request["assignee"] = None
+                pull_request.assignee = pr["assignees"]
 
             if "code Review" in pr:
-                pull_request["code review"] = pr["code Review"]
-            else:
-                pull_request["code review"] = None
+                pull_request.codeReview = pr["code Review"]
 
             if "sciTech Review" in pr:
-                pull_request["scitech review"] = pr["sciTech Review"]
-            else:
-                pull_request["scitech review"] = None
+                pull_request.scitechReview = pr["sciTech Review"]
 
-            repo = pr["content"]["repository"].replace("MetOffice/", "")
-            data[repo].append(pull_request)
+            pull_request.repo = pr["content"]["repository"].replace("MetOffice/", "")
+            data[pull_request.repo].append(pull_request)
 
         return data, milestones
 
@@ -136,11 +134,11 @@ class ProjectData:
             print("\n=== Reviewers for " + repo)
 
         for pr in pull_requests:
-            sr = pr["scitech review"]
+            sr = pr.scitechReview
             if sr:
                 reviewers.append(sr)
 
-            cr = pr["code review"]
+            cr = pr.codeReview
             if cr:
                 reviewers.append(cr)
 
@@ -156,7 +154,7 @@ class ProjectData:
                     f"{sr: <18}",
                     "Code:",
                     f"{cr: <18}",
-                    pr["title"],
+                    pr.title,
                 )
 
         return reviewers
@@ -180,14 +178,56 @@ class ProjectData:
         for repo in self.data:
             for pr in self.data[repo]:
                 if (
-                    pr["status"] == status
+                    pr.status == status
                     or status == "all"
-                    or (status == "open" and pr["status"] in self.open_states)
-                    or (status == "closed" and pr["status"] not in self.open_states)
+                    or (status == "open" and pr.status in self.open_states)
+                    or (status == "closed" and pr.status not in self.open_states)
                 ):
-                    milestone = pr["milestone"]
+                    milestone = pr.milestone
                     if not milestone_data[milestone]:
                         milestone_data[milestone] = defaultdict(list)
                     milestone_data[milestone][repo].append(pr)
 
         return milestone_data
+
+    def archive_milestone(self, milestone: str, dry_run: bool = False) -> None:
+
+        print(f"Archiving all completed pull requests for {milestone}")
+
+        dry_run = dry_run | self.test  # if test data, or a dryrun, then dummy commands
+
+        closed_prs = self.get_by_milestone(status="closed")[milestone]
+        for repo in closed_prs:
+            for pr in closed_prs[repo]:
+                pr.archive(dry_run)
+
+
+class PullRequest:
+
+    def __init__(self, id: str = None, number: str = None, title: str = None):
+        self.id = id
+        self.number = number
+        self.title = title
+
+        self.repo = None
+        self.status = None
+        self.milestone = "None"
+        self.assignee = None
+        self.scitechReview = None
+        self.codeReview = None
+
+    def archive(self, dry_run: bool = False):
+        """
+        Archive this pull request from the project.
+
+        dry_run: If true, print the command used rather than archiving.
+        """
+
+        command = f"gh project item-archive {project_id} --owner {project_owner} --id {self.id}"
+        message = f"Archiving #{self.number} in {self.repo}"
+
+        if dry_run:
+            print(f"[DRY RUN] {message: <40} {command}")
+        else:
+            print(message)
+            run_command(command)
