531ffdccd1e764e8e46db56d43d0b8f8eedf74ad
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2026-02-03 11:43:57 +0000

    bug

--- github_scripts/get_git_sources.py
+++ github_scripts/get_git_sources.py
@@ -351,7 +351,7 @@ def sync_repo(repo_source: str, repo_ref: str, loc: Path) -> None:
         result = run_command(f"ssh {host} git -C {path} status --ignored -s")
     except ValueError:
         # In case the path does not contain `host:` - see if it can be accessed locally
-        result = run_command(f"git -C {path} status --ignored -s")
+        result = run_command(f"git -C {repo_source} status --ignored -s")
     for ignore_file in result.stdout.split("\n"):
         ignore_file = ignore_file.strip()
         if not ignore_file.startswith("!!"):
