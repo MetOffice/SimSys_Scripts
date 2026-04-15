e2f64df1f75f555f786eef3b1e1760a1518d90fe
R Sharp <r-sharp@users.noreply.github.com>
2026-04-15 13:00:34 +0100

    Adding a copyright statement
    Co-authored-by: Erica Neininger <107684099+ericaneininger@users.noreply.github.com>

--- script_umdp3_checker/tests/test_umdp3_conformance.py
+++ script_umdp3_checker/tests/test_umdp3_conformance.py
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------------------------
+# (C) Crown copyright Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+# -----------------------------------------------------------------------------
+
 import sys
 import subprocess
 from pathlib import Path
