6afb284cddf03d743e7c13458cdd982167886400
r-sharp <roddy.sharp@metoffice.gov.uk>
2026-04-15 13:05:01 +0100

    Adding other missing copyright statements

--- script_umdp3_checker/umdp3_conformance.py
+++ script_umdp3_checker/umdp3_conformance.py
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------------------------
+# (C) Crown copyright Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+# -----------------------------------------------------------------------------
+
 import subprocess
 from abc import ABC, abstractmethod
 from pathlib import Path
