a0568b16938d9f8abcd0f334bed0e15de311b869
R Sharp <r-sharp@users.noreply.github.com>
2026-04-21 09:44:42 +0100

    Add basic pytest tests for umdp3_conformance.py (#216)
    * Yet another weird round of having to repeatedly accept the same changes which git / VS Code are marking as in conflict.
    Annoyingly, this seems to have introduced a bug as well.
    
    * Fixing issue with external runners not establishing properly.
    
    * Fixing an error that seems to have crept in during merging main and resolving clashes. But looking at it, I really can't fathom out 'how'
    
    * Where do these line length errors in the linter(s) come from - I haven't edited this file...
    
    * quick "improvement" to the error reporting in one of the tests
    
    * Why am I constantly having to re-impliment fixes/tidying I'm sure I've done before.
    
    * And now ruff_format wants to change them back again...
    
    * Add some basic tests
    
    * Using suggestions from CoPilot, inseret what it considers the high priority checks missing from umdp3_conformance.py. That was fun
    
    * ScoobyDoobyDOOOOoooooooo
    
    * Minor tweak to output text that seems to have got lost in previous branches.
    
    * Tweaking the tests and taking suggestions from CoPilot for further tests - I think the power has gone to it's head...
    
    * adapting to the latest crazy linting settings
    
    * move the type ignore comment which the autoformatter shifted to the next line where it no longer works...
    
    * Oh look ma - the linter didn't like the change made to appease the formatter, or was it the other way round...
    
    * Adding a copyright statement
    
    Co-authored-by: Erica Neininger <107684099+ericaneininger@users.noreply.github.com>
    
    * Adding other missing copyright statements
    
    ---------
    
    Co-authored-by: Erica Neininger <107684099+ericaneininger@users.noreply.github.com>

--- script_umdp3_checker/tests/test_fortran_checks.py
+++ script_umdp3_checker/tests/test_fortran_checks.py
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------------------------
+# (C) Crown copyright Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+# -----------------------------------------------------------------------------
+
 import pytest
 import sys
 from pathlib import Path
