f1d79b341094bcfdbf0a4dcdc86305a07de2cd2a
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2026-04-29 15:25:09 +0100

    bump timeout

--- lfric_macros/apply_macros.py
+++ lfric_macros/apply_macros.py
@@ -41,7 +41,7 @@ def run_command(command, shell=False):
         command,
         capture_output=True,
         text=True,
-        timeout=120,
+        timeout=300,
         shell=shell,
         check=False,
     )
